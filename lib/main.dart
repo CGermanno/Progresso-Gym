7421140223e93812588dd6da473738d6f5414f56
CGermanno <carlosg.gasparello@gmail.com>
2025-02-15 18:24:16 -0300

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,21 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_receitas2/telas/autenticacaoTela.dart';
-//import 'package:flutter_application_receitas2/telas/exercicio.dart';
+
+import 'package:flutter_application_receitas2/telas/exercicio.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    DevicePreview(
+      enabled: true,
+      builder: (context) => MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
