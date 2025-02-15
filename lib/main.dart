774010c1632f6168f13214a804cb17227b8eaf90
CGermanno <carlosg.gasparello@gmail.com>
2025-02-15 17:53:07 -0300

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_receitas2/telas/autenticacaoTela.dart';
-import 'package:flutter_application_receitas2/telas/exercicio.dart';
+//import 'package:flutter_application_receitas2/telas/exercicio.dart';
 
 void main() {
   runApp(const MyApp());
